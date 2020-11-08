11829ef27014675ad189f83512dbb7f4cd690b23
maximilienmoraud <maximillien.moraud@etu.imt-lille-douai.fr>
2020-11-08 15:31:41 +0100

    c fini sauf la partie de mehdi

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def nbrDeVariable():
     temp = input(
         "Entrez les noms de vos variables en les séparants par une virgule:\n(N'oubliez pas les contraites)\n")
     temp = temp.split(',')
-    return temp
+    return np.array(temp)
 
 
 def contraintes(X):
@@ -14,27 +14,32 @@ def contraintes(X):
     contrainteTab = []
     A = [[] for i in range(nbrContrainte)]
     B = []
+    print(
+        "-Entrez les coefficiant devant chaque variable dans l'ordre en les séparants par un espace et ajoutez en denier (toujours en séparant par un espace) le membre droit de l'inéquation:\n(Exemple: 3x+5y<=10 => 3 5 10)")
     for i in range(nbrContrainte):
-        print("\nContrainte numéro " + str(i) + ":")
-        for j in range(len(X) - nbrContrainte):
-            temp = float(input("Entrez le coefficiant devant la variable " + X[j] + ": "))
-            A[i].append(temp)
-        temp = float(input("Entrez le membre droit de l'inéquation\n(Exemple: Si 3x-5y<=4, entrez 4): "))
-        B.append(temp)
+        temp = input("Contrainte numéro " + str(i) + "\n")
+        temp = [float(x) for x in temp.split()]
+        for j in range(len(temp) - 1):
+            A[i].append(temp[j])
+        B.append(temp[-1])
     v = np.ones(nbrContrainte)
     diag = np.diag(v)
+    print(A)
     A = np.concatenate((A, diag), axis=1)
-    return A, B
+    return np.array(A), np.array(B)
 
 
 def fonctionAMaximiser(X, B):
     C = []
-    for i in range(len(X) - len(B)):
-        temp = float(input("Entrez le coefficiant devant la variable " + X[i] + " de la fonction à maximiser: "))
-        C.append(temp)
+    print(
+        "Entrez les coefficiants devant chaque variable de la fonction à maximiser en les séparants par un espace:\n(Exemple: Z=3x+2y => 3 2)\n ")
+    temp = input()
+    temp = [float(x) for x in temp.split()]
+    for e in temp:
+        C.append(-e)
     while len(C) != len(X):
         C.append(0)
-    return C
+    return np.array(C)
 
 
 def simplex(X, A, B, Z, Base):
@@ -70,16 +75,25 @@ def simplex(X, A, B, Z, Base):
 
 
 if __name__ == '__main__':
-    # X = nbrDeVariable()
-    # A, B = contraintes(X)
-    # C = fonctionAMaximiser(X, B)
-    X = np.array(['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7'])
-    A = np.array([[10, 15, 11, 12, 0, 8, 1, 0, 0, 0, 0, 0, 0], [12, 12, 12, 0, 12, 15, 0, 1, 0, 0, 0, 0, 0],
-                  [0, 12, 13, 16, 0, 4.57, 0, 0, 1, 0, 0, 0, 0], [5, 6, 0, 0, 7.5, 6, 0, 0, 0, 1, 0, 0, 0],
-                  [2, 1, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0], [10, 0, 10, 12, 8, 10, 0, 0, 0, 0, 0, 1, 0],
-                  [0, 0, 12, 12, 14, 16, 0, 0, 0, 0, 0, 0, 1]])
-    B = np.array([900, 1000, 700, 500, 150, 800, 800])
-    C = np.array([2.1, 2.2, 1.8, 1.2, 1.6, 2.2, 0, 0, 0, 0, 0, 0, 0])
-    defaultbase = np.copy(X[len(A[:, 1]) - 1:])
-
-    simplex(X, A, B, -C, defaultbase)
+    #X = np.array(['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7'])
+    #A = np.array([[10, 15, 11, 12, 0, 8, 1, 0, 0, 0, 0, 0, 0], [12, 12, 12, 0, 12, 15, 0, 1, 0, 0, 0, 0, 0],
+    #              [0, 12, 13, 16, 0, 4.57, 0, 0, 1, 0, 0, 0, 0], [5, 6, 0, 0, 7.5, 6, 0, 0, 0, 1, 0, 0, 0],
+    #              [2, 1, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0], [10, 0, 10, 12, 8, 10, 0, 0, 0, 0, 0, 1, 0],
+    #              [0, 0, 12, 12, 14, 16, 0, 0, 0, 0, 0, 0, 1]])
+    #B = np.array([900, 1000, 700, 500, 150, 800, 800])
+    #C = np.array([2.1, 2.2, 1.8, 1.2, 1.6, 2.2, 0, 0, 0, 0, 0, 0, 0])
+    #defaultbase = np.copy(X[len(A[:, 1]) - 1:])
+    #simplex(X, A, B, -C, defaultbase)
+    #print("X", X)
+    #print("A", A)
+    #print("B", B)
+    #print("C", C)
+    X1 = nbrDeVariable()
+    A1, B1 = contraintes(X1)
+    C1 = fonctionAMaximiser(X1, B1)
+    defaultbase1 = np.copy(X1[len(A1[:, 1]) - 1:])
+    print("X1", X1)
+    print("A1", A1)
+    print("B1", B1)
+    print("C1", C1)
+    simplex(X1, A1, B1, C1, defaultbase1)
